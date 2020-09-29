747810258cd40c6004e77c90be19af4d7fdc5be6
James Gideon <jamgid@yahoo.com>
2020-09-28 21:30:59 -0400

    random pass gen working with slider for length of pass

--- James-Gideon-msu-fsf-Homework3/script.js
+++ James-Gideon-msu-fsf-Homework3/script.js
@@ -1,14 +1,30 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generate() {
+  //set password length  
+  let complexity = document.getElementById("slider").value;
+  //set password values
+  let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+  
+  let password = "";  
+  //loop to pick specified amount of characters
+  for(var i = 0; i <= complexity; i++){
+      password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
+  //add password to display area
+  document.getElementById("display").value = password;
 
-  passwordText.value = password;
+  
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+document.getElementById("length").innerHTML = "Length: 68";
+
+document.getElementById("slider").oninput = function(){
+    
+    if(document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;    
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: 8"
+    }
+}
